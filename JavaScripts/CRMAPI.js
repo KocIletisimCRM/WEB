3a4f0d8ea77296e3bde9b28c798bb36bdd4af4e1
ozalgun <ozal.algun@gmail.com>
2015-04-24 13:29:56 +0300

    ilk a

---
+++ JavaScripts/CRMAPI.js
@@ -0,0 +1,25 @@
+﻿if (!window.CRMAPI) {
+    window.CRMAPI = (function () {
+        baseURI = "http://localhost:50752/api";
+        return {
+            getTaskQueues: function (pageNo, rowsPerPage, filter, onsuccess, onerror) {
+
+                $.ajax({
+                    url: baseURI + "/Task/getTasks",
+                    method: "POST",
+                    contentType: "application/json",
+                    data: JSON.stringify({ pageNo: pageNo, rowsPerPage: rowsPerPage, filter: filter })
+
+                })
+                .fail(function () {
+                    if (onerror) onerror();
+                })
+                .success(function (a, b, c) {
+                    if (onsuccess) onsuccess(a, b, c);
+                })
+                .complete(function () {
+                })
+            }
+        };
+    })();
+}
---
+++ JavaScripts/app.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="~/JavaScripts/jquery-2.1.3.js" />
+/// <reference path="~/JavaScripts/CRMAPI.js" />
+/// <reference path="~/JavaScripts/knockout-3.3.0.js" />
+var rowCount = 25;
+$(window).load(function () {
+    $("#btn").click(function () {
+        CRMAPI.getTaskQueues(10, rowCount, {}, function (a, b, c) {
+            taskqueueList(a);
+        }, function () { });
+    });
+    $("#f1").change( function () {
+        CRMAPI.getTaskQueues(10, rowCount, { TaskIds: null, TaskName:$("#f1").val(), TaskTypeIds: null, TaskTypeName: null }, function (a, b, c) {
+            taskqueueList(a);
+        }, function () { });
+    });
+    $('#rowCount').on('change', function () {
+        rowCount = this.value;
+    });
+    taskqueueList = ko.observableArray([]);
+    ko.applyBindings(taskqueueList, $("#Task")[0]);
+})
