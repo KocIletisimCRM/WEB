c3353ea82bd19a4d0e7e457dfe0df7f61408392c
kociletisim <yazilim@kociletisim.com.tr>
2015-06-06 15:12:05 +0300

    güncelleme

---
+++ JavaScripts/CustomerMV.js
@@ -0,0 +1,92 @@
+﻿/// <reference path="~/JavaScripts/jquery-2.1.3.js" />
+/// <reference path="~/JavaScripts/CustomerTransferApi.js" />
+/// <reference path="~/JavaScripts/knockout-3.3.0.js" />
+
+$(window).load(function () {
+
+    var ct = new customerTransfer();
+    ko.applyBindings(ct);
+    ct.getCustomer();
+    ct.getSite();
+});
+
+
+var customerTransfer = function () {
+    var self = this;
+    this.selectedSiteValue = ko.observable();
+    this.selectedBlockValue = ko.observable();
+    this.selectedFlatValue = ko.observable();
+    this.flat = ko.observable();
+    this.customerid = null;
+    this.control = ko.observable();
+    self.cname = ko.observable();
+    this.resultmessage = ko.observable();
+    this.cList = ko.observableArray([]);
+    this.sList = ko.observableArray([]);
+    this.bList = ko.observableArray([]);
+    this.flatList = ko.observableArray([]);
+    this.selectedSiteValue.subscribe(function (v) {
+
+        customerblock.findBlock(v, function (a, b, c) { self.bList(a); }, null);
+    });
+    this.selectedBlockValue.subscribe(function (v) {
+        findFlat.findflat(v, function (a, b, c) { self.flatList(a); }, null);
+    });
+}
+
+customerTransfer.prototype.select = function(d, e){
+    $("#customer tr").removeClass("selected");
+    $(e.currentTarget).addClass("selected");
+   this.customerid = d.customerid;
+}
+
+customerTransfer.prototype.getCustomer = function (d,e) {
+    var self = this;
+    if (e&&(e.which==1||e.which==13)) {
+        CustomerTransferApi.findCustomer(self.cname(), function (a, b, c) { self.cList(a); }, null);
+    }
+    return true;
+};
+
+customerTransfer.prototype.getSite = function () {
+    var self = this;
+    customersite.findSite(function (a, b, c) { self.sList(a); }, null);
+};
+
+customerTransfer.prototype.toJSON = function () {
+
+    var self = this;
+    var obj= {
+        customerid: self.customerid,
+        blockid: self.selectedBlockValue(),
+        flat:self.selectedFlatValue(),
+    };
+    if (obj.customerid==null||obj.blockid==null||obj.flat==null) {
+        alert("Eksik veya yanlış bilgi girdiniz.");
+        return null;
+    }
+    else {
+    saveTransfer.savetransfer(obj.customerid,obj.blockid,obj.flat, function (a, b, c) { self.sList(a); }, null);
+    console.log(obj);
+    var url = "http://crmapitest.kociletisim.com.tr:8083";
+    $(location).attr('href', url);
+    return obj;
+        }
+
+};
+
+customerTransfer.prototype.pasiveCustomer = function () {
+    var self = this;
+    if (self.customerid==null) {
+        alert("Eksik veya yanlış bilgi girdiniz.Müşteriyi Seçiniz");
+        return null;
+    }
+    pasiveCustomer.pasiveCustomer(self.customerid, function (a, b, c) { self.resultmessage(a); }, null);
+    return self.resultmessage();
+};
+
+
+
+
+
+
---
+++ JavaScripts/CustomerTransferApi.js
@@ -0,0 +1,163 @@
+﻿if (!window.CustomerTransferApi) {
+
+  baseURI = "http://localhost:50752/api"
+  // baseURI = "http://crmapitest.kociletisim.com.tr:8083/api"
+    window.CustomerTransferApi = (function () {
+
+        return {
+            findCustomer: function (cname, onsuccess, onerror) {
+
+                $.ajax({
+                    url: baseURI + "/Nakil/findCustomer?cname=" + cname,
+                    method: "GET",
+                    contentType: "application/json",
+
+
+                })
+                    .fail(function () {
+                        if (onerror) onerror();
+                    })
+                    .success(function (a, b, c) {
+                        if (onsuccess) onsuccess(a, b, c);
+                    })
+                    .complete(function () {
+                    })
+
+            }
+        };
+
+    }
+    )();
+
+    window.customersite = (function () {
+
+
+        return {
+            findSite: function (onsuccess, onerror) {
+
+                $.ajax({
+                    url: baseURI + "/Nakil/findSite",
+                    method: "GET",
+                    contentType: "application/json",
+
+
+                })
+                    .fail(function () {
+                        if (onerror) onerror();
+                    })
+                    .success(function (a, b, c) {
+                        if (onsuccess) onsuccess(a, b, c);
+                    })
+                    .complete(function () {
+                    })
+
+            }
+
+        };
+    }
+
+    )();
+
+    window.customerblock = (function () {
+
+
+        return {
+            findBlock: function (siteid, onsuccess, onerror) {
+                $.ajax({
+                    url: baseURI + "/Nakil/findBlock?siteid=" + siteid,
+                    method: "GET",
+                    contentType: "application/json",
+
+
+                })
+                    .fail(function () {
+                        if (onerror) onerror();
+                    })
+                    .success(function (a, b, c) {
+                        if (onsuccess) onsuccess(a, b, c);
+                    })
+                    .complete(function () {
+                    })
+            }
+        };
+    }
+
+    )();
+
+    window.findFlat = (function () {
+
+        return {
+            findflat: function (blockid, onsuccess, onerror) {
+
+                $.ajax({
+                    url: baseURI + "/Nakil/findFlat?blockid=" + blockid,
+                    method: "GET",
+                    contentType: "application/json",
+
+
+                })
+                    .fail(function () {
+                        if (onerror) onerror();
+                    })
+                    .success(function (a, b, c) {
+                        if (onsuccess) onsuccess(a, b, c);
+                    })
+                    .complete(function () {
+                    })
+            }
+
+        };
+
+
+    })();
+    
+
+    window.saveTransfer = (function () {
+        return {
+            savetransfer: function (customerid,blockid,flat, onsuccess, onerror) {
+                $.ajax({
+                    url: baseURI + "/Nakil/saveTransfer",
+                    method: "POST",
+                    contentType: "application/json",
+                    data: JSON.stringify({ customerid: customerid, blockid: blockid, flat: flat })
+
+
+                })
+                    .fail(function () {
+                        if (onerror) onerror();
+                    })
+                    .success(function (a, b, c) {
+                        if (onsuccess) onsuccess(a, b, c);
+                    })
+                    .complete(function () {
+                    })
+            }
+        };
+    }
+
+
+    )();
+
+    window.pasiveCustomer = (function () {
+
+        return {
+            pasiveCustomer: function (custid, onsuccess, onerror) {
+                $.ajax({
+                    url: baseURI + "/Nakil/pasiveCustomer?custid="+custid,
+                    method: "GET",
+                    contentType: "application/json",
+                    
+                })
+                    .fail(function () {
+                        if (onerror) onerror();
+                    })
+                    .success(function (a, b, c) {
+                        if (onsuccess) onsuccess(a, b, c);
+                    })
+                    .complete(function () {
+                    })
+            }
+        };
+    })();
+
+}
